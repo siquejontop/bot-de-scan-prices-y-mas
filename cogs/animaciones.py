092ad51892b040df7bf96a8f0323c812e69e39aa
Juan <angelpro1234la@gmail.com>
2025-10-30 03:38:15 -0500

    Refactor kiss command to use local GIF file
    Removed unnecessary HTTP request for GIF and simplified kiss command.

--- cogs/animaciones.py
+++ cogs/animaciones.py
@@ -1,32 +1,21 @@
 # cogs/kiss.py
 import discord
 from discord.ext import commands
-import aiohttp
-import io
 
 class Kiss(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
-        self.session = aiohttp.ClientSession()
 
     @commands.command()
     async def kiss(self, ctx, member: discord.Member):
         if member == ctx.author:
             return await ctx.send("No te beses a ti mismo.")
+        if member.bot:
+            return await ctx.send("No puedes besar a un bot.")
 
-        url = "https://i.imgur.com/7z3x8fF.gif"  # GIF 100% FUNCIONAL
         text = f"**{ctx.author.display_name}** besó a **{member.display_name}**"
-
-        try:
-            async with self.session.get(url) as resp:
-                if resp.status == 200:
-                    data = await resp.read()
-                    file = discord.File(fp=io.BytesIO(data), filename="kiss.gif")
-                    await ctx.send(text, file=file)
-                else:
-                    await ctx.send(text + "\n*(GIF no disponible)*")
-        except:
-            await ctx.send(text + "\n*(Error al enviar GIF)*")
+        file = discord.File("kiss.gif", filename="kiss.gif")
+        await ctx.send(text, file=file)
 
 async def setup(bot):
     await bot.add_cog(Kiss(bot))
